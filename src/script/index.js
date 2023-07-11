4065dfc521d5cafb57a0010f9f562156d8ba794b
Nellya <syunyaeva5555@gmail.com>
2023-07-11 17:27:44 +0400

    добавила слайдер

---
+++ src/script/index.js
@@ -0,0 +1,15 @@
+// js-swiper-portfolio
+
+const swiper1 = new Swiper('.js-swiper-portfolio', {
+    slidesPerView: 1,
+    loop: true,
+    spaceBetween: 50,
+    // pagination: {
+    //   el: '.swiper-pagination',
+    //   clickable: true,
+    // },
+    // navigation: {
+    //   nextEl: '.swiper-button-next',
+    //   prevEl: '.swiper-button-prev',
+    // },
+  });
